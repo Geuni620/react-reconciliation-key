e0f3d55010fd5b6f4cc487a4fa9bc8ccbe6dde64
Geuni <dlrmsgnl620@naver.com>
2024-03-24 21:07:02 +0900

    react reconciliation key
    - https://x.com/dai_shi/status/1415283957910831106\?s\=20

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,67 @@
-import { Button } from 'src/components/ui/button';
+import { useState } from 'react';
 
 export const App = () => {
   return (
-    <div className="absolute left-10 top-0">
-      <Button>Click me</Button>
+    <div>
+      <h3>ComponentWithoutKey</h3>
+      <ComponentWithoutKey />
+
+      <br />
+
+      <h3>ComponentWithKey</h3>
+      <ComponentWithKey />
+    </div>
+  );
+};
+
+const shuffle = <T,>(arr: T[]): T[] => {
+  const arrWithRand: [T, number][] = arr.map((x) => [x, Math.random()]);
+  arrWithRand.sort((a, b) => a[1] - b[1]);
+
+  return arrWithRand.map((x) => x[0]);
+};
+
+const Counter = ({ init }: Item) => {
+  const [count, setCount] = useState(init);
+  return (
+    <div>
+      (state: {count} <button onClick={() => setCount((c) => c + 1)}>+1</button>
+      ) (init: {init})
+    </div>
+  );
+};
+
+type Item = { init: number };
+
+const ComponentWithoutKey = () => {
+  const [arr, setArr] = useState<Item[]>([
+    { init: 1 },
+    { init: 2 },
+    { init: 3 },
+  ]);
+  return (
+    <div>
+      <button onClick={() => setArr(shuffle)}>shuffle</button>
+      <hr />
+      {arr.map((item, idx) => (
+        <Counter key={idx} init={item.init} />
+      ))}
+    </div>
+  );
+};
+const ComponentWithKey = () => {
+  const [arr, setArr] = useState<Item[]>([
+    { init: 1 },
+    { init: 2 },
+    { init: 3 },
+  ]);
+  return (
+    <div>
+      <button onClick={() => setArr(shuffle)}>shuffle</button>
+      <hr />
+      {arr.map((item) => (
+        <Counter key={item.init} init={item.init} />
+      ))}
     </div>
   );
 };
